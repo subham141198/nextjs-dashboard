e25a37c46566919930871ffe41c60e608157c3a8
Subham Banerjee <banerjee.subham1411@gmail.com>
2024-03-06 19:03:10 +0530

    Streaming completed

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -37,7 +37,7 @@ export async function fetchLatestInvoices() {
   noStore();
   try {
 
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
